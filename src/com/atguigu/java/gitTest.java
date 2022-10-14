d9996a8879bfac42e7ab22dc06bf089bc3332a54
ziqian <2297652577@qq.com>
2022-10-14 22:34:51 +0800

    pull commit

--- src/com/atguigu/java/gitTest.java
+++ src/com/atguigu/java/gitTest.java
@@ -13,5 +13,6 @@ public class gitTest {
         System.out.println("Hello git3");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("pull test");
     }
 }
